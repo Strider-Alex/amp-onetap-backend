140704e3f8b47f9274acefbec044f5e638266040
Kefan Yang <kefany@google.com>
2019-06-13 14:26:59 -0700

    support rp domain onetap page

--- app.js
+++ app.js
@@ -1,58 +1,81 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const cors = require('cors');
+const url = require('url');
+const jwtDecode = require('jwt-decode');
+const app = express();
+const port = 3000;
+
+const upload = multer();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(cookieParser())
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 
-const whitelist = ['http://localhost:8000', 'http://ampcache.com:8000']
+const whitelist = ['http://localhost:3000', 'http://localhost:8000', 'http://ampcache.com:8000'];
 let _origin = null;
 const corsOptions = {
   origin: function (origin, callback) {
-    _origin = origin
+    _origin = origin;
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      console.log('Not allowed: ' + origin);
+      callback(new Error('Not allowed by CORS'));
     }
   },
   methods: 'GET',
   credentials: true
-}
+};
 
 
 app.get('/amp-access', cors(corsOptions), (req, res) => {
-  res.setHeader('AMP-Access-Control-Allow-Source-Origin', _origin)
-  res.setHeader('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin')
-  if(req.cookies.tokenID){
-    res.json({user:true})
+  res.setHeader('AMP-Access-Control-Allow-Source-Origin', _origin);
+  res.setHeader('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin');
+  if (req.cookies.tokenID) {
+    const decoded = jwtDecode(req.cookies.tokenID);
+    res.json({ user: true, given_name: decoded.given_name, email: decoded.email });
   }
-  else{
-    res.json({user:false})
+  else {
+    res.json({ user: false  });
   }
-})
-
-app.post('/', cors(corsOptions), (req, res) => {
-  console.log(req.body.credential);
-  res.cookie('tokenID', req.body.credential);
-  res.redirect('back');
-})
-
-app.get('/logout', (req, res) => {
-  if(req.cookies.tokenID){
-    res.clearCookie('tokenID')
-  }
-  res.redirect(`${req.query.return}#success=true`); 
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post('/login-xhr', [cors(corsOptions), upload.none()], (req, res) => {
+  res.cookie('tokenID', req.body.credential);
+  res.json({ success: 1 });
+});
+
+app.get('/logout', (req, res) => {
+  if (req.cookies.tokenID) {
+    res.clearCookie('tokenID');
+  }
+  res.redirect(`${req.query.return}#success=true`);
+});
+
+app.get('/onetap', (req, res) => {
+  let origin;
+  const referer = req.get('Referer');
+  if (referer) {
+    const q = url.parse(referer, true);
+    origin = `${q.protocol}//${q.host}`;
+  }
+  res.set('X-Frame-Options', 'DENY');
+  if (whitelist.indexOf(origin) !== -1) {
+    res.set({
+      'Content-Security-Policy': `frame-ancestors ${origin}`,
+      'X-Frame-Options': `ALLOW-FROM ${origin}`,
+    });
+  }
+  res.render('onetap');
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
